4af2e1556bc6de02b7da29d01a55b7aafe97376b
timmy <81864321+timmythesheep1@users.noreply.github.com>
2022-02-08 00:41:50 +0300

    Update Auth.php

--- src/Auth/Auth.php
+++ src/Auth/Auth.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace Auth;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\{Command, CommandSender};
+
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\utils\MainLogger as M;
+
+use pocketmine\Player;
+
+use Auth\FormAPI\CustomForm;
+
+class Auth extends PluginBase implements Listener 
+{
+    /** @var array */
+    public $login = [];
+    
+    public function onEnable()
+	{
+                M::getLogger()->info("Auth1 plugin enabled by timmydev1");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+}
